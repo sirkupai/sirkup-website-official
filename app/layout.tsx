ed12ba2e63e3170da06f796947b808482918d5fd
Rimsha Rafique <69843275+Rimsharafique11@users.noreply.github.com>
2025-10-06 14:28:55 +0500

    meta pixels added

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,15 +37,39 @@ export default function RootLayout({
             });
           `}
         </Script>
+
+        {/* ✅ Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '4209918145993152');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=4209918145993152&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
+
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem={true}
+          enableSystem
           disableTransitionOnChange
           storageKey="sirkup-theme"
-          forcedTheme={undefined}
         >
           {/* Analytics listener to capture SPA route changes */}
           <AnalyticsListener />
